be39ddd3969ba6589cc2ea1acf5a96edfb8307aa
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-10 19:53:06 +0000

    Shift bypass only_visible

--- Reddit/Reddit_autohide.user.js
+++ Reddit/Reddit_autohide.user.js
@@ -1,34 +1,42 @@
 // ==UserScript==
 // @name        Reddit_AutoHide
 // @namespace   Reddit
-// @description Hide all articles with F2
+// @description Hide all articles with F2/shift-F2
 // @include     https://*.reddit.com/*
-// @version     1.05
+// @version     1.05b
 // @grant       none
 // ==/UserScript==
 //1=true,0=false
 var DEBUG=true;//log debug info
 var log_only=false;//true->don't hide just log debug info
-var only_visible=true;//hide only visible articles
+var only_visible=true;//hide only visible articles,Shift-F2 to bypass
 var hide_interval=500;//milliseconds
 var auto_refresh=true;//reload if all articles hidden (only_visible=false)
 var auto_next=true;//load next page if all visible articles hidden.(only_visible=true)
-var keyname='F2'//key to use
+var keyname='F2';//key to use, 
+var isshift=0;//track bypass only_visible Shift-F2; setting to 1 will ignore only_visible
+
+
 //================================
 
 var process_article_counter=0;
 function dbg(text){if(DEBUG){console.info(text)};}
-function isHidden(el) {    return (el.offsetParent === null)}
-function hidepress(e){if(e.code!=keyname)return;hider();}
+function isHidden(el) {    return (el.offsetParent === null);}
+function hidepress(e){if(e.code!=keyname)return;
+isshift=e.shiftKey;hider();}
 
 function handle_end(){dbg("Page hiding complete, total hidden;"+process_article_counter);
-if(only_visible && auto_next && !log_only){document.querySelector('.nav-buttons>.nextprev>.next-button>a').click();}
-if(!only_visible && auto_refresh && !log_only){document.location.href=document.location.href;} return 0;}
+if(log_only)return;
+if(!auto_next &&!auto_refresh)return;//no further actions
+if(only_visible && !isshift && auto_next ){//normal next
+document.querySelector('.nav-buttons>.nextprev>.next-button>a').click();return;}
+if(auto_refresh&& (!only_visible||isshift)){
+document.location.href=document.location.href; };return 0;}
 
 
 function process_article(h){
 h.setAttribute('processed-hidden',1);
-if(only_visible && isHidden(h))return 0;
+if(!isshift && only_visible && isHidden(h))return 0;
 var linkdata=h.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.querySelector('a[data-event-action="title"]');
 dbg("Hiding link:"+linkdata);
 if(!log_only)h.click();process_article_counter++;
