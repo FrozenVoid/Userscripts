05b03de4e90ace87c196d5c281f5e51cdf4a0475
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-11-27 09:00:04 +0200

    Create new file

---
+++ Github/Github_NotifyFlash.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Github_NotifyFlash
+// @namespace   Github
+// @description notification flash on every open window title
+// @include     https://github.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+function check(){
+var a=document.querySelector('a.notification-indicator');
+if(a.getAttribute('aria-label').search("You have no unread notifications")==-1){
+document.title=Math.random()>0.5?"NEW NOTES":"";
+}
+}
+setInterval(check,1000);
