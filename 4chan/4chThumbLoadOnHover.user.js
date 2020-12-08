4724fb013f1b8f8f0174c6f7a4ab44ea6e21c04d
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-12-08 10:58:29 +0200

    Create 4chThumbLoadOnHover.user.js

---
+++ 4chan/4chThumbLoadOnHover.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        4chThumbLoadOnHover
+// @namespace   4ch
+// @description load Thumbs on Hover
+// @include     https://boards.4chan.org/*
+// @include     https://boards.4channel.org/*
+// @version     1.03
+// @grant       none
+// ==/UserScript==
+function th_load(){
+var thumbs=document.querySelectorAll('.fileThumb:not([onmouseover])>img:not(.fileDeletedRes)')
+
+for(var i=0;i<thumbs.length;i++){
+thumbs[i].setAttribute("onmouseover","this.click()")}
+window.setTimeout(th_load,4000);}
+th_load()
+
