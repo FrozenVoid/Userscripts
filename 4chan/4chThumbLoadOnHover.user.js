e647b8c58b552e5bfe9e4d5ccaeb40c6ab5d20c9
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-12-08 13:21:07 +0200

    Update 4chThumbLoadOnHover.user.js

--- 4chan/4chThumbLoadOnHover.user.js
+++ 4chan/4chThumbLoadOnHover.user.js
@@ -1,19 +1,19 @@
-
 // ==UserScript==
 // @name        4chThumbLoadOnHover
 // @namespace   4ch
-// @description load Thumbs on Hover(hover out twice to unload)
+// @description load Thumbs on Hover
 // @include     https://boards.4chan.org/*
 // @include     https://boards.4channel.org/*
-// @version     1.03
+// @version     1.04
 // @grant       none
 // ==/UserScript==
 function th_load(){
 var thumbs=document.querySelectorAll('.fileThumb:not([onmouseover])>img:not(.fileDeletedRes)')
 
 for(var i=0;i<thumbs.length;i++){
-thumbs[i].setAttribute("onmouseover","this.click()")}
-window.setTimeout(th_load,4000);}
-th_load()
+thumbs[i].setAttribute("onmouseover","this.click();")}
 
+}
+window.addEventListener('DOMSubtreeModified',th_load)
+th_load();
 
