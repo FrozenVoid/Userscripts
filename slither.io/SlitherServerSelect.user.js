791fe85a03f1366eee15a6c52a4aac7415040cc3
FrozenVoid <FrozenVoid@users.noreply.github.com>
2020-12-09 20:10:36 +0200

    autoreconnect

--- slither.io/SlitherServerSelect.user.js
+++ slither.io/SlitherServerSelect.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name     SlitherServerSelect
-// @version  1.03
+// @version  1.04
 // @author FrozenVoid
 // @description Server select when F2 is pressed
 // @include   http://slither.io/
 // @grant    unsafeWindow
 // ==/UserScript==
+var autorecconect=1;//enable autoreconnent to selected server.
 
 const selectbutton="F2";//key name
 const clusters=["Unknown",1,"Germany","Canada","Singapore",
@@ -16,17 +17,27 @@ const clusters=["Unknown",1,"Germany","Canada","Singapore",
               17,"US East 4" ,
               19,20,21,22,23,24,25,26,27,28,29];
 
+var sselected=false;
 
+function reco(){
+  if(unsafeWindow.connected&&unsafeWindow.playing)
+  return;
+  if(!sselected||!autorecconect)return;
+unsafeWindow.connect(); }
+setInterval(reco,400)
 function setserver(server){
-unsafeWindow.forcing = true;
+unsafeWindow.forcing = true;sselected=true;
 for(var i in unsafeWindow.sos){
 if(i==server)continue;
 unsafeWindow.sos.ac=1;}
 unsafeWindow.sos[server].ac=9999;
-unsafeWindow.bso=unsafeWindow.sos[server];}
+unsafeWindow.bso=unsafeWindow.sos[server];
+
+unsafeWindow.connect();
+}
 
 function buildslist(){var s="";
-if(unsafeWindow.playing||!unsafeWindow.sos||!unsafeWindow.sos.length)return "Waiting for server list";
+if(unsafeWindow.playing||!unsafeWindow.sos||!unsafeWindow.sos.length)return "Waiting for server list/already connected";
 for(var i in unsafeWindow.sos){
 if(!unsafeWindow.sos[i])continue;
 s+=
