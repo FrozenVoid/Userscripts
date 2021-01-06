5158f685a3a5e14c4b66cb447ebc4022f7328b0e
FrozenVoid <FrozenVoid@users.noreply.github.com>
2021-01-06 08:25:34 +0200

    Update Youtube_keys.user.js

--- Youtube/Youtube_keys.user.js
+++ Youtube/Youtube_keys.user.js
@@ -1,21 +1,18 @@
 // ==UserScript==
 // @name         Youtube_Keys
 // @namespace    YT
-// @version      1.01d
+// @version      1.01e
 // @description  F2 to next video.
 // @author       FrozenVoid
 // @match        https://www.youtube.com/watch?v*
 // @grant       none
 // ==/UserScript==
 
-    function evproc(ev){
+
+window.addEventListener("keypress",function (ev){
       if(ev.key=="F2"){var c=document.querySelector(
           "a.ytd-compact-video-renderer");if(c)c.click();}
-
-
-    }
-
-   window.addEventListener("keypress",evproc);
+    });
 
 
 
