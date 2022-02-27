c4514e2605edb4d5c5651d7b44b14f35e786b0c3
FrozenVoid <FrozenVoid@users.noreply.github.com>
2022-02-27 13:57:43 +0200

    Create imgur_redir.user.js

---
+++ Imgur/imgur_redir.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Imgur_Redir
+// @namespace   proxy
+// @description Redirect Imgur links
+// @include     https://imgur.com/*
+// @version     1
+// @grant       none
+// @run-at document-start
+// ==/UserScript==
+document.location.href=document.location.href.replace("imgur.com","imgurp.com");
